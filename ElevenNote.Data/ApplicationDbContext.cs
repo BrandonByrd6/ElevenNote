a12fa66256ae60a4ae465de3f3ca006168eb38f1
Brandon Byrd <brandon.r.byrd6@gmail.com>
2023-12-28 10:36:47 -0500

    update Program.cs

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-namespace ElevenNote.Data
+namespace ElevenNote.Data;
+
+public class ApplicationDbContext : IdentityDbContext<UserEntity, IdentityRole<int>, int>
 {
-    public class ApplicationDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
         
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserEntity>().ToTable("Users");
+    } 
+
 }
--- ElevenNote.Data/Entities/UserEntity.cs
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -7,8 +7,8 @@ public class UserEntity : IdentityUser<int>
 {
     [MaxLength(100)]
     public string? FirstName {get; set;}
+    
     [MaxLength(100)]
-
     public string? LastName {get; set;}
 
     [Required]
